a0454f1f027fb49de2b7866a3d016fcb53d1d25c
ivand <ivand@DESKTOP-OPFIMC0>
2020-11-14 11:34:26 +0100

    IDP Imagenes y cookies

--- public_html/paginaPrincipal.js
+++ public_html/paginaPrincipal.js
@@ -1,30 +1,22 @@
-            let nodoIntentos = document.querySelector('#intentos');
-            let nodoHistorial = document.querySelector('#historial');
-            var escritas = prompt("Escribe las palabras con las que quieres jugar").split(",");  
-
-            window.open("PaginaDibuix.html", "Dibuix", "top=25, left=1100, width=400, height=200");
-            window.open("PaginaEstadisticas.html", "Estadisticas", "top=350, left=1100, width=400, height=200");
-            window.open("PaginaParaula.html", "Paraula", "top=575, left=600, width=400, height=200");
-
-            function arrayPalabras(){
-                if(escritas == ""){
-                   escritas = ["desenvolupament","web","nicolau","copernic"];
-                }console.log(escritas);                
+            function getCookie(cname) {
+                var name = cname + "=";
+                var decodedCookie = decodeURIComponent(document.cookie);
+                var ca = decodedCookie.split(";");
+                for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == " ") {
+                    c = c.substring(1);
                 }
-            arrayPalabras();
-            
-            function prepararJuego () {
-                //// 1 Selecciono una palabra aleatoria de listaPalabra
-                //// 1.1 Obtengo la posicion aleatoria
-                let posAleatoriaListaPalabras = _.random(escritas.length - 1);
-                //// 1.2 Obtengo la palabra aleatoria
-                let palabraAleatoria = escritas[posAleatoriaListaPalabras];
-                //// 1.3 Separo la palabra en letras y lo guardo
-                palabraAdivinar = palabraAleatoria.split('');
-                //// 2 Preparo el array que va a ver el usuario. Tendrá el mismo número de guiones que letras en palabraAdivinar
-                for (let letra of palabraAdivinar) {
-                    palabraMostrar.push('_ ');
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
                 }
-                //// 3 Dibuja todo lo necesario
-                dibujarJuego();
             }
+                return "";
+        }
+                function setCookie(cname, cvalue, exdays) {
+                    var d = new Date();
+                    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+                    var expires = "expires=" + d.toUTCString();
+                    document.cookie = cname + "=" + cvalue + ";" + expires;
+                }
+                console.log(document.cookie);   
